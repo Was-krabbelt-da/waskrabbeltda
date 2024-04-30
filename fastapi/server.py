bfa7d7e24cd40225a8d39cd2c3c39cab171feba1
phoeinx <hradilak@uni-potsdam.de>
2024-04-30 14:25:58 +0200

    Add start_time and end_time to endpoint, store with correct filename.

--- fastapi/server.py
+++ fastapi/server.py
@@ -1,9 +1,10 @@
 # import io
 
 from pathlib import Path
+import shutil
 from typing import Annotated
 from datetime import datetime
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, UploadFile
 import pandas as pd
 import os
 from prediction.yolov5.classify.predict import run as run_classification
@@ -25,20 +26,28 @@ data = pd.read_csv("new_classification_data.csv")
 
 #TODO: Continue working here!
 #TODO: Make async
-#TODO: Receive filename and timestamp
+#TODO: Checkout if we can get all timestamps without dpeending on naming
 #TODO: checkout what top1_prob_mean is and how to get it
 @app.post("/classify/{tracking_id}")
-async def classify(tracking_id: int, files: Annotated[list[bytes], File(timestamp=datetime)]):
+async def classify(tracking_id: int, files: list[UploadFile]):
     data_path = Path("data", str(tracking_id))
-    print(data_path)
-    data_path.mkdir(exist_ok=True)
-    for i, file in enumerate(files):
-        with open(data_path / f"{i}.jpg", "wb") as f:
-            f.write(file)
+    data_path.mkdir(exist_ok=True)#TODO: exist_ok logic
+    timestamps = []
+    for file in files:
+        date_string = file.filename.split(".")[0]
+        timestamp = datetime.strptime(date_string, "%Y%m%d_%H-%M-%S")
+        timestamps.append(timestamp)
+        file_path = data_path / file.filename
+        with open(file_path, "wb+") as file_object:
+            shutil.copyfileobj(file.file, file_object) 
+    #convert timestamps to datetime
+    timestamps.sort()
     #get json object with date,time,track_ID,track_ID_imgs,top1,top1_prob
     classification_results = run_classification(tracking_id=tracking_id)
-    new_row = {'date': datetime.today().date(),
-               'time': datetime.now().time(),
+    new_row = {'date': datetime.now().date(),
+               'start_time': timestamps[0],
+                'end_time': timestamps[-1],
+                'duration_s': (timestamps[-1] - timestamps[0]).total_seconds(),
                'track_ID': tracking_id,
                'track_ID_imgs': len(files),
                'top1': classification_results["top1"],
