b35b0f31ab51230f44f4a134046cee5cc1b93072
phoeinx <hradilak@uni-potsdam.de>
2024-05-12 10:47:31 +0200

    Update deployment for persistent storage.

--- fastapi/server.py
+++ fastapi/server.py
@@ -9,11 +9,18 @@ from fastapi.security.api_key import APIKey
 
 from prediction.yolov5.classify.predict import run as run_classification
 
+CLASSIFICATION_DATA_PATH = Path(".", "data", "classification_data.csv")
+
 app = FastAPI(
     title="WasKrabbeltDa? - Backend",
     description="""Obtain data from your local insect-detect camera.""",
     version="0.0.1",
 )
+
+# Setup
+if not CLASSIFICATION_DATA_PATH.exists():
+    data = pd.DataFrame(columns=["date", "start_time", "end_time", "duration_s", "track_ID", "track_ID_imgs", "top1", "top1_prob"])
+    data.to_csv(CLASSIFICATION_DATA_PATH, index=False)
 
 @app.post("/classify/{tracking_id}")
 async def classify(
@@ -47,13 +54,15 @@ async def classify(
             'top1': classification_results["top1"],
             'top1_prob': classification_results["top1_prob"],
             }
-    data = pd.read_csv("classification_data.csv")
+    
+
+    data = pd.read_csv(CLASSIFICATION_DATA_PATH)
     data.loc[len(data)] = new_row
-    data.to_csv("classification_data.csv", index=False)
+    data.to_csv(CLASSIFICATION_DATA_PATH, index=False)
     
     return {"success": True}
 
 @app.get("/data")
 def read_root(api_key: APIKey = Depends(auth.get_api_key)):
-    data = pd.read_csv("classification_data.csv")
+    data = pd.read_csv(CLASSIFICATION_DATA_PATH)
     return data.to_dict(orient="records")
